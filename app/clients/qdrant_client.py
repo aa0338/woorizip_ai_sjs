ffb26d96f514eaf51e23f4cde2ed6e5bb24b4dbc
Jae-Sang <aa0338@naver.com>
2026-03-12 04:44:10 +0900

    오류수정uuid

--- app/clients/qdrant_client.py
+++ app/clients/qdrant_client.py
@@ -1,6 +1,7 @@
 # app/clients/qdrant_client.py
 
-from uuid import uuid4, uuid5
+from uuid import uuid4, uuid5, UUID
+import uuid
 
 from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct
@@ -35,7 +36,7 @@ class QdrantDbClient:
         
         points=[]
         for i in range(0, n):
-            point_id = str(uuid5(uuid4, f"{target.roomNo}-{i}"))
+            point_id = str(uuid5(UUID(target.roomNo), f"{i}"))
             points.append(PointStruct(id=point_id, vector=vector[i], payload={"roomNo": target.roomNo}))
         
         info = self.client.upsert(
