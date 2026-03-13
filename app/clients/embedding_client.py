ee885f51b89c735aa3061237c9624704a81ac06c
Jae-Sang <aa0338@naver.com>
2026-03-13 10:11:37 +0900

    .

--- app/clients/embedding_client.py
+++ app/clients/embedding_client.py
@@ -2,24 +2,24 @@
 
 from sentence_transformers import SentenceTransformer
 
-from app.core.config import settings
-from openai import OpenAI
+# from app.core.config import settings
+# from openai import OpenAI
 
 
 
 # text-embedding-3-small client 생성
-class OpenaiEmbeddingClient:
-    def __init__(self):
-        self.client=OpenAI(api_key=settings.OPENAI_API_KEY)
-        self.model_name="text-embedding-3-small"
+# class OpenaiEmbeddingClient:
+#     def __init__(self):
+#         self.client=OpenAI(api_key=settings.OPENAI_API_KEY)
+#         self.model_name="text-embedding-3-small"
         
-    def embed(self, text:str):
-        response=self.client.embeddings.create(
-            input=text,
-            model=self.model_name
-        )
-        vector=response.data[0].embedding
-        return vector
+#     def embed(self, text:str):
+#         response=self.client.embeddings.create(
+#             input=text,
+#             model=self.model_name
+#         )
+#         vector=response.data[0].embedding
+#         return vector
     
 # nlpai-lab/KURE-v1
 class KureEmbeddingClient:
